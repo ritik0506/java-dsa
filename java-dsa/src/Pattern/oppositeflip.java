5bd29f24ecd75f1fab1c0f7583b27f5a5708691f
Ritik Kumar <154360423+ritik0506@users.noreply.github.com>
2025-11-21 12:01:40 +0530

    "add on patterns"

---
+++ java-dsa/src/Pattern/oppositeflip.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class oppositeflip {
+
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            System.out.println("enter the number");
+            int n= sc.nextInt();
+            for (int i=1;i<=n;i++){
+                for (int k=1;k<=i;k++){
+                    System.out.print(" ");
+                }
+                for (int j=1;j<=n-i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
